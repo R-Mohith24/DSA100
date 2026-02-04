7f800a379e07b50409c9fc80fa979a87d5f94860
Mohith <mohith02412@gmail.com>
2026-02-04 22:20:21 +0530

    completed trapping rainwater sum using 2 pointer approach

--- LL_ARRAYS/trappingRainWater.py
+++ LL_ARRAYS/trappingRainWater.py
@@ -7,7 +7,7 @@ Output: 6
 Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
 In this case, 6 units of rain water (blue section) are being trapped
 """
-
+# watch tutorial from NEETCODE
 class Solution(object):
     def trap(self, height):
 
